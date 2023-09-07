86519b7d350a023180fda8cc29eb0c05855afc77
surya0827 <144340335+surya0827@users.noreply.github.com>
2023-09-07 17:00:46 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,21 @@
-num = 2
+# Python program to check if year is a leap year or not
 
-factorial = 1
-if num < 0:
-  print("Sorry, factorial does not exist for negative numbers")
-elif num == 0:
-  print("The factorial of 0 is 1")
+year = 2000
+
+# To get year (integer input) from the user
+# year = int(input("Enter a year: "))
+
+# divided by 100 means century year (ending with 00)
+# century year divided by 400 is leap year
+if (year % 400 == 0) and (year % 100 == 0):
+    print("{0} is a leap year".format(year))
+
+# not divided by 100 means not a century year
+# year divided by 4 is a leap year
+elif (year % 4 ==0) and (year % 100 != 0):
+    print("{0} is a leap year".format(year))
+
+# if not divided by both 400 (century year) and 4 (not century year)
+# year is not leap year
 else:
-  for i in range(1, num + 1):
-    factorial = factorial * i
-  print("The factorial of", num, "is", factorial)
+    print("{0} is not a leap year".format(year))
